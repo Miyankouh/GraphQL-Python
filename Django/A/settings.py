10e6f016444dddcc8a7b91f6403e0c593d44ae0c
miyankouh <majid.miyankouh@gmail.com>
2022-08-22 12:01:22 +0430

    add: home & graphene to INSTALLED_APPS

--- Django/A/settings.py
+++ Django/A/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # internal apps
+    'home.apps.HomeConfig',
+    # external apps
+    "graphene_django"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,9 @@ ROOT_URLCONF = 'A.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
